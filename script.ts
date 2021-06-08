e98c603a650641b95079c0400ba085c663527f1a
Marq Gray <marquis.s.sandifer@gmail.com>
2021-06-08 15:10:22 -0500

    Imported some elements and created placeholder calculate function

---
+++ script.ts
@@ -0,0 +1,9 @@
+const currencyElementOne = document.getElementById("currency-one");
+const amountElementOne = document.getElementById("amount-one");
+const currencyElementTwo = document.getElementById("currency-two");
+const amountElementTwo = document.getElementById("amount-two");
+
+const rateElement = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+const calculate = () => {};
