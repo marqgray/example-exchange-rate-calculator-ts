d86c2ae88406616c9d5e432dfa953b8836142ac1
Marq Gray <marquis.s.sandifer@gmail.com>
2021-06-08 15:11:44 -0500

    Bound event listeners

--- script.ts
+++ script.ts
@@ -7,3 +7,8 @@ const rateElement = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
 const calculate = () => {};
+
+currencyElementOne.addEventListener("change", calculate);
+amountElementOne.addEventListener("input", calculate);
+currencyElementTwo.addEventListener("change", calculate);
+amountElementTwo.addEventListener("input", calculate);
