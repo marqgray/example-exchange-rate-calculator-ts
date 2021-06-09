067ec2349607989a98db0f130aeb726eb23e4d4e
Marq Gray <marquis.s.sandifer@gmail.com>
2021-06-08 19:40:17 -0500

    Finished calculate implementation

--- script.ts
+++ script.ts
@@ -14,7 +14,23 @@ const amountElementTwo: HTMLInputElement = <HTMLInputElement>(
 const rateElement = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
-const calculate = () => {};
+const calculate = () => {
+  const currencyOne = currencyElementOne.value;
+  const currencyTwo = currencyElementTwo.value;
+
+  fetch(
+    `https://v6.exchangerate-api.com/v6/27c9f4cf9d5b967c980b0e5e/latest/${currencyOne}`
+  )
+    .then((res) => res.json())
+    .then((data: ExchangeRateApiResult) => {
+      const rate = Number(data.conversion_rates[currencyTwo]);
+      rateElement.innerText = `1 ${currencyOne} = ${rate} ${currencyTwo}`;
+
+      amountElementTwo.value = (Number(amountElementOne.value) * rate).toFixed(
+        2
+      );
+    });
+};
 
 currencyElementOne.addEventListener("change", calculate);
 amountElementOne.addEventListener("input", calculate);
